1dc2859e22fbeb57748f3bd87b5518b497f0fe3a
monoei <24331@tun.ac.th>
2025-06-22 03:42:27 +0000

    add new file

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,7 @@
+number = float(input("Enter a number: "))
+if number < 0:
+    print("This number is negative.")
+elif number > 0:
+    print("this number is postitive.")
+else:
+    print("This number is both positive and negative.")
